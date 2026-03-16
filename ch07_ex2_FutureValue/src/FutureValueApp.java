a22d6f03e363f1674a7b5390cd437d09ced036f3
Terrell Taylor <ttaylo59@smartweb.augustatech.edu>
2026-03-16 14:05:13 -0400

    commit

--- ch07_ex2_FutureValue/src/FutureValueApp.java
+++ ch07_ex2_FutureValue/src/FutureValueApp.java
@@ -11,16 +11,16 @@ public class FutureValueApp {
         while (choice.equalsIgnoreCase("y")) {
             // get the input from the user
             System.out.println("DATA ENTRY");
-            double monthlyInvestment = getDouble(sc,
+            double monthlyInvestment = Console.getDouble(sc,
                     "Enter monthly investment: ", 0, 1000);
-            double interestRate = getDouble(sc,
+            double interestRate = Console.getDouble(sc,
                     "Enter yearly interest rate: ", 0, 30);
-            int years = getInt(sc,
+            int years = Console.getInt(sc,
                     "Enter number of years: ", 0, 100);
             System.out.println();
 
             // calculate the future value
-            double futureValue = calculateFutureValue(
+            double futureValue = FinancialCalculations.calculateFutureValue(
                     monthlyInvestment, interestRate, years);
 
             // get the currency and percent formatters
@@ -44,68 +44,5 @@ public class FutureValueApp {
             choice = sc.nextLine();
             System.out.println();
         }
-    }
-    
-    public static double getDouble(Scanner sc, String prompt) {
-        while (true) {
-            System.out.print(prompt);
-            try {
-                return Double.parseDouble(sc.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.println("Error! Invalid decimal value.");
-            }
-        }
-    }
-
-    public static double getDouble(Scanner sc, String prompt,
-            double min, double max) {
-        while (true) {
-            double value = getDouble(sc, prompt);
-            if (value > min && value < max) {
-                return value;
-            } else {
-                System.out.println("Error! Number must be greater than " + 
-                        min + " and less than " + max + ".");
-            }
-        }
-    }
-    
-    public static int getInt(Scanner sc, String prompt) {
-        while (true) {
-            System.out.print(prompt);
-            try {
-                return Integer.parseInt(sc.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.println("Error! Invalid integer value.");
-            }
-        }
-    }
-    
-    public static int getInt(Scanner sc, String prompt,
-            int min, int max) {
-        while (true) {
-            int value = getInt(sc, prompt);
-            if (value > min && value < max) {
-                return value;
-            } else {
-                System.out.println("Error! Number must be greater than " + 
-                        min + " and less than " + max + ".");
-            }
-        }
-    }
-
-    public static double calculateFutureValue(double monthlyInvestment,
-            double interestRate, int years) {
-        
-        // convert yearly values to monthly values
-        double monthlyInterestRate = interestRate/12/100;
-        int months = years * 12;
-            
-        double futureValue = 0.0;
-        for (int i = 1; i <= months; i++) {
-            futureValue = (futureValue + monthlyInvestment) *
-                          (1 + monthlyInterestRate);
-        }
-        return futureValue;
     }
 }
